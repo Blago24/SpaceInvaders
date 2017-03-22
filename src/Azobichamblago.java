fce9a28ab7ecec6b66d9f2ad56b31816099f694f
Blago24 <blago102@gmail.com>
2017-03-22 11:17:20 +0200

    no message

--- src/Azobichamblago.java
+++ src/Azobichamblago.java
@@ -1,15 +1,16 @@
 
 public class Azobichamblago {
-<<<<<<< HEAD
+
 	//love 
 	public static void test(){
 		
 	System.out.println("pesho");
-=======
+}
+
 	
 	
 	public static void main(String[] args) { 
 		System.out.println("test");
->>>>>>> 56eca99b127794e3ad585f8a756f748b865d3216
+test();
 	}
 }
