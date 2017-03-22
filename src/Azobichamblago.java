0d3e865f96156588b8f759b1599bce10705b2990
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-03-22 11:19:24 +0200

    tryada

--- src/Azobichamblago.java
+++ src/Azobichamblago.java
@@ -1,16 +1,22 @@
 
 public class Azobichamblago {
 
-	//love 
-	public static void test(){
-		
-	System.out.println("pesho");
-}
+	// love
+	public static void test() {
 
-	
-	
-	public static void main(String[] args) { 
-		System.out.println("test");
-test();
+		System.out.println("pesho");
 	}
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		test();
+		countNumbers();
+	}
+	
+	public static void countNumbers()  { 
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+	
 }
