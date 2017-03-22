19ce4e1a5de9d972667db27094628c167a721a20
Blago24 <blago102@gmail.com>
2017-03-22 14:29:05 +0200

    no message

--- desktop/src/com/vratsasoftware/spaceinvaders/desktop/DesktopLauncher.java
+++ desktop/src/com/vratsasoftware/spaceinvaders/desktop/DesktopLauncher.java
@@ -2,14 +2,11 @@ package com.vratsasoftware.spaceinvaders.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.vratsasoftware.spaceinvaders.SpaceInvaders;
+import com.vratsasoftware.spaceinvaders.runGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SpaceInvaders(), config);
-		config.width = 600;
-		config.height = 800;
-		config.title = "Space Invaders";
+		new LwjglApplication(new runGame(), config);
 	}
 }
