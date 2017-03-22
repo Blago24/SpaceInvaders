20e1ffe28d9d807ca77b28726b43f9fe475737ed
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-03-22 16:06:26 +0200

    Set an icon to the game.

--- desktop/src/com/vratsasoftware/spaceinvaders/desktop/DesktopLauncher.java
+++ desktop/src/com/vratsasoftware/spaceinvaders/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.vratsasoftware.spaceinvaders.desktop;
-
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.vratsasoftware.spaceinvaders.SpaceInvaders;
@@ -8,6 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new SpaceInvaders(), config);
+		
+		config.addIcon("img\\gameIcon.png", Files.FileType.Internal);
 		config.width = 600;
 		config.height = 800;
 		config.title = "Space Invaders";
