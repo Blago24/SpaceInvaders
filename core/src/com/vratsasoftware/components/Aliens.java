a134b6ec0db2185d200e588f8940f8a7e431272a
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-03-26 19:31:05 +0300

    Added a function of keeping the coordinates of every single alien throughout the game and a funtion that checks if an alien is alive.

--- core/src/com/vratsasoftware/components/Aliens.java
+++ core/src/com/vratsasoftware/components/Aliens.java
@@ -9,37 +9,66 @@ public class Aliens {
 
 	private final int ALIENS_PER_ROW = 11;
 	private final int ALIENS_ROWS = 5;
-	private Vector2 aliensPosition;
+	
 	public Aliens[][] aliens;
-	private Texture alien;
 	public float alienX;
 	public float alienY;
+	
+	
+	private Vector2 aliensPosition;
+	private Texture alien;
+	private int[][] aliensValue; 
+	private int[][] aliensCoordinatesX;
+	private int[][] aliensCoordinatesY;
+	
 
 	public Aliens() {
 		aliens = new Aliens[ALIENS_ROWS][ALIENS_PER_ROW];
+		aliensValue = new int[ALIENS_ROWS][ALIENS_PER_ROW];
+		aliensCoordinatesX = new int[ALIENS_ROWS][ALIENS_PER_ROW];
+		aliensCoordinatesY = new int[ALIENS_ROWS][ALIENS_PER_ROW];
 		alien = new Texture("images//aliens.png");
 	}
 
+	//TODO Try adding an animation to the alien 
+	
 	protected void showAliens(SpriteBatch batch) {
 		int aliensX = Gdx.graphics.getWidth() - (Gdx.graphics.getWidth() - 50);
 		int aliensXHolder = aliensX;
 		int aliensY = Gdx.graphics.getHeight() - 100;
 		for (int i = 0; i < aliens.length; i++) {
 			for (int j = 0; j < aliens[0].length; j++) {
-				batch.draw(aliens[i][j].getAlien(), aliensX, aliensY, 35, 35);
+				//check if the alien is alive
+				if (isAlienAlive(i, j)) {
+					batch.draw(aliens[i][j].getAlien(), aliensX, aliensY, 35, 35);
+					//keep the X and Y coordinates for each alien 
+					// would probably use it to detect collision
+					aliensCoordinatesX[i][j] = aliensX;
+					aliensCoordinatesY[i][j] = aliensY;
+				}
 				aliensX += 45;
 			}
 			aliensX = aliensXHolder;
 			aliensY -= 50;
 		}
 	}
+	
 
-	// TODO Generate a moveAliens() method;
+	private boolean isAlienAlive(int i, int j) {
+		return aliensValue[i][j] == 1;
+	}
 
 	protected void createNewAliens() {
 		for (int i = 0; i < aliens.length; i++) {
 			for (int j = 0; j < aliens[0].length; j++) {
 				aliens[i][j] = new Aliens();
+				/* 
+				 * set the default value of the alien to 1
+				 * if changed to a different number (e.g. 0) 
+				 * the alien won't be displayed on the screen. 
+				 */
+				aliensValue[i][j] = 1; 
+				//test to see if working
 			}
 		}
 	}
