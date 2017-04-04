9e3c2ab5d60d6cdf403e1438ffaef11bd5fadb80
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-04-05 00:16:41 +0300

    Added a Highscore screen.

--- core/src/Screens/HighscoreScreen.java
+++ core/src/Screens/HighscoreScreen.java
@@ -1,28 +1,112 @@
 package Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g3d.particles.ResourceData.SaveData;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeBitmapFontData;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.vratsasoftware.spaceinvaders.SpaceInvaders;
 
 public class HighscoreScreen implements Screen, InputProcessor {
 
-	
-	private boolean newHighscore; 
+	private final int BACKGROUND_WIDTH = SpaceInvaders.SCREEN_WIDTH + 100;
+	private final int BACKGROUND_HEIGHT = SpaceInvaders.SCREEN_HEIGHT;
+	private boolean newHighscore;
 	private char[] newName;
-	
+
+	private FreeTypeFontGenerator generator;
 	private BitmapFont gameOverFont;
-	private BitmapFont font;
 	
+
+	private Texture backButton;
+	private Texture background;
+	private Texture backgroundTwo;
+	String text = "Highscores \n 1: \n 2: \n 3:";
+	private int firstBgX;
+	private int firstBgY;
+
+	private int secondBgX;
+	private int secondBgY;
+
 	private SpriteBatch batch;
-	
 
 	@Override
 	public void show() {
+
+		this.backButton = new Texture("images//back-button.png");
+		this.background = new Texture("images//highScoreBackground.png");
+		this.backgroundTwo = new Texture("images//highScoreBackground.png");
+		FileHandle fh = new FileHandle("assets//adrip1.fnt");
+		this.generator = new FreeTypeFontGenerator(Gdx.files.local("assets//adrip1.ttf")); 
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+	    parameter.size = 96;
+	    parameter.color = Color.CORAL;
+		gameOverFont = generator.generateFont(parameter);
+		generator.dispose();
+		this.batch = new SpriteBatch();
+		this.firstBgX = SpaceInvaders.SCREEN_WIDTH - this.background.getWidth() / 2 - 20;
+		this.firstBgY = 0;
+		this.secondBgX = SpaceInvaders.SCREEN_WIDTH - this.background.getWidth() / 2 - 20;
+		this.secondBgY = firstBgY - SpaceInvaders.SCREEN_HEIGHT;
 		
-		
+
 	}
+
+	@Override
+	public void render(float delta) {
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		moveBackground(this.batch);
+		
+		if (moveBackground(this.batch)) {
+			firstBgY--;
+			secondBgY--;
+		} else {
+			System.out.println(firstBgY);
+			firstBgY++;
+			secondBgY++;
+		}
+		
+		
+		batch.draw(backgroundTwo, secondBgX, secondBgY, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+		batch.draw(background, firstBgX, firstBgY, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+		batch.draw(backButton, 59, 50, 50, 50);
+		gameOverFont.draw(batch, text, SpaceInvaders.SCREEN_WIDTH/2 - 300, SpaceInvaders.SCREEN_HEIGHT - 250);
+		// working;
+		// TODO Implement the high score results;
+		// Would probably need to use a hash map
+		batch.end();
+
+	}
+
+	private boolean moveBackground(SpriteBatch batch) {
+
+		if (firstBgY > 888) {
+			return true;
+		} else {
+
+			return false;
+		}
+
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		float pointerX = InputTransform.getCursorToModelX(SpaceInvaders.SCREEN_WIDTH, screenX);
+		float pointerY = InputTransform.getCursorToModelY(SpaceInvaders.SCREEN_HEIGHT, screenY);
+
+		return false;
+	}
+
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
@@ -37,12 +121,6 @@ public class HighscoreScreen implements Screen, InputProcessor {
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -71,43 +149,34 @@ public class HighscoreScreen implements Screen, InputProcessor {
 		return false;
 	}
 
-
-	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
 
 }
--- core/src/Screens/MenuScreen.java
+++ core/src/Screens/MenuScreen.java
@@ -19,20 +19,20 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 
 	private float buttonX;
 	private float buttonY;
-	
+
 	private float highscoreX;
-	private float highscoreY; 
-	
+	private float highscoreY;
+
 	private boolean displayScreen;
 
 	private SpriteBatch batch;
 	private Texture background;
 	private Texture playButton;
 	private Texture logo;
-	private Texture highscore;
-	
+	private Texture highscoreButton;
+
 	private Music menuMusic;
-	
+
 	private Vector2 logoCoordinates;
 
 	Game game;
@@ -47,18 +47,17 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 		this.background = new Texture("images//backgr.png");
 		this.playButton = new Texture("images//play-button.png");
 		this.logo = new Texture("images//vsc-logo.png");
-		this.highscore = new Texture("images//highscore.png");
+		this.highscoreButton = new Texture("images//highscore.png");
 		this.batch = new SpriteBatch();
-		this.logoCoordinates = new Vector2(0,0);
+		this.logoCoordinates = new Vector2(0, 0);
 		buttonX = (this.WIDTH / 2) - (playButton.getWidth() / 2 - 75);
 		buttonY = this.HEIGHT / 2 - 135;
-		highscoreX = ((this.WIDTH / 2) - (highscore.getWidth() / 2 - 205));
+		highscoreX = ((this.WIDTH / 2) - (highscoreButton.getWidth() / 2 - 205));
 		highscoreY = this.HEIGHT / 2 - 100;
 		menuMusic = Gdx.audio.newMusic(Gdx.files.local("assets//backgroundMusic.ogg"));
 		menuMusic.setLooping(true);
 		menuMusic.play();
 		Gdx.input.setInputProcessor(this);
-		render(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
@@ -66,17 +65,18 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 		batch.begin();
 		batch.draw(background, -30, 0, this.WIDTH, this.HEIGHT);
 		batch.draw(playButton, buttonX, buttonY, 150, 150);
-		batch.draw(highscore, highscoreX, highscoreY, 75, 75);
+		batch.draw(highscoreButton, highscoreX, highscoreY, 75, 75);
 		animateLogo(this.batch);
 		if (displayScreen) {
 			batch.dispose();
+			Gdx.gl20.glClearColor(1, 0, 0, 1);
 		}
 		batch.end();
 	}
 
 	private void animateLogo(SpriteBatch batch2) {
 		batch.draw(logo, logoCoordinates.x, logoCoordinates.y, this.WIDTH, this.HEIGHT);
-		logoCoordinates.y += 5; //2.5;
+		logoCoordinates.y += 10; // 2.5;
 	}
 
 	@Override
@@ -84,13 +84,54 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 		float pointerX = InputTransform.getCursorToModelX(this.WIDTH, screenX);
 		float pointerY = InputTransform.getCursorToModelY(this.HEIGHT, screenY);
 
+		System.out.println("Highscore: " + pointerX);
+		System.out.println("Y " + pointerY);
+		//
 		if (isButtonPressed(pointerX, pointerY)) {
 			openGameScreen();
+		} else if (highscorePress(pointerX, pointerY)) {
+			openHighscoreScreen();
+		}
+
+		return false;
+	}
+
+	private void openHighscoreScreen() {
+		game.setScreen(new HighscoreScreen());
+	}
+
+	private boolean isHighScorePressed(float pointerX, float pointerY) {
+		int highscoreTopY = 450;
+		int highscoreLeftX = 540;
+		int highscoreBottomY = 360;
+		int highscoreRightX = 630;
+
+		if ((pointerY >= highscoreBottomY && pointerY <= highscoreTopY)
+				&& (pointerX >= highscoreLeftX && pointerX <= highscoreRightX)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean highscorePress(float pointerX, float pointerY) {
+		if (isHighScorePressed(pointerX, pointerY)) {
+			if (displayScreen == false) {
+				return true;
+			}
 		}
 		return false;
 	}
 
 	private boolean isButtonPressed(float pointerX, float pointerY) {
+		if (isPlayButtonPressed(pointerX, pointerY)) {
+			if (displayScreen == false) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isPlayButtonPressed(float pointerX, float pointerY) {
 		int buttonTopY = 480;
 		int buttonBottomY = 400;
 		int buttonLeftX = 450;
@@ -98,11 +139,9 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 
 		if ((pointerY >= buttonBottomY && pointerY <= buttonTopY)
 				&& (pointerX >= buttonLeftX && pointerX <= buttonRightX)) {
-			if (displayScreen == false) {
-				return true;
-			}
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	private void openGameScreen() {
@@ -114,13 +153,11 @@ public class MenuScreen extends SpaceInvaders implements Screen, InputProcessor 
 		displayScreen = true;
 		return false;
 	}
-	
 
 	public void dispose() {
 		this.logo.dispose();
 		this.background.dispose();
 		this.playButton.dispose();
-		
 	}
 
 	@Override
